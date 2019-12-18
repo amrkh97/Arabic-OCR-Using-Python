e27a5ac46463927647255672c62ac29b59c662f1
amrkh97 <amrkh97@gmail.com>
2019-12-18 10:53:13 +0200

    Added Correct Skew

--- feature_extractor.py
+++ feature_extractor.py
@@ -3,6 +3,20 @@ import numpy as np
 from scipy import stats
 
 
+def correct_skew(thresh):
+    coords = np.column_stack(np.where(thresh > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+	    angle = -(90 + angle)
+    else:
+	    angle = -angle
+    
+    (h, w) = thresh.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(thresh, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return rotated
+
 def getVerticalProjection(img):
     return (np.sum(img, axis = 0))//255
 
