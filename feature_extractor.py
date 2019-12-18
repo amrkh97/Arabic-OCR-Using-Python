ee9c42f6014fc63929adaaa26a58c6ce180c08fa
amrkh97 <amrkh97@gmail.com>
2019-12-18 10:59:11 +0200

    Added Detecting words using contours

--- feature_extractor.py
+++ feature_extractor.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 from scipy import stats
 
-
 def correct_skew(thresh):
     coords = np.column_stack(np.where(thresh > 0))
     angle = cv2.minAreaRect(coords)[-1]
@@ -24,4 +23,24 @@ def getHorizontalProjection(img):
     return (np.sum(img, axis = 1))//255
 
 def DetectWords(line_img):
-    pass
+    input_image = correct_skew(line_img) 
+    _,thresh = cv2.threshold(input_image, 127, 255,cv2.THRESH_OTSU|cv2.THRESH_BINARY_INV)
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    dilation = cv2.dilate(thresh, rect_kernel, iterations = 1)
+    contours_initial, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    
+    Detected_Words = []
+    # sort the contours
+    for method in ("right-to-left","top-to-bottom"):
+	    contours_initial, boundingBoxes = contours.sort_contours(contours_initial, method=method)
+    for cnt in contours_initial:
+            x, y, w, h = cv2.boundingRect(cnt)
+            fx = x+w
+            fy = y+h
+            
+            trial_image = input_image[y:fy,x:fx]
+            trial_image[trial_image < 255] = 0
+            trial_image = 255 - trial_image
+            Detected_Words.append(trial_image)
+    return Detected_Words
+
